328af53a390c643ed6a21ce0560418c81b271a8e
Shashank2985 <mrxyzdarkness3435@gmail.com>
2025-08-01 18:53:12 +0530

    Authentication added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,10 @@
 import Image from "next/image";
-
 export default function Home() {
   return (
     <div className="flex items-center justify-center">
       <h1 className="text-3xl">
         Welcome to the NextJS Project
       </h1>
-    </div>
+   </div>
   );
 }
---
+++ app/components/Navbar.tsx
@@ -0,0 +1,70 @@
+import Link from "next/link";
+import Image from "next/image";
+import { auth, signOut, signIn } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/app/components/ui/avatar";
+
+const Navbar = async () => {
+  const session = await auth();
+  console.log("SESSION:", session);
+
+  return (
+    <header className="px-5 py-3 bg-white shadow-sm font-work-sans">
+      <nav className="flex justify-between items-center">
+        <Link href="/">
+          <Image src="/logo.png" alt="logo" width={144} height={30} />
+        </Link>
+
+        <div className="flex items-center gap-5 text-black">
+          {session?.user ? (
+            <>
+              <Link href="/startup/create" className="flex items-center gap-1">
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
+              </Link>
+
+              <form
+                action={async () => {
+                  "use server";
+                  await signOut({ redirectTo: "/" });
+                }}
+              >
+                <button type="submit" className="flex items-center gap-1">
+                  <span className="max-sm:hidden">Logout</span>
+                  <LogOut className="size-6 sm:hidden text-red-500" />
+                </button>
+              </form>
+
+              <Link
+                href={`/user/${session?.user?.id || session?.user?.email}`}
+                className="flex items-center gap-3"
+              >
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session.user.image || ""}
+                    alt={session.user.name || "User"}
+                  />
+                  <AvatarFallback>
+                    {session.user.name?.[0] || "U"}
+                  </AvatarFallback>
+                </Avatar>
+                <span className="max-sm:hidden">{session.user.name}</span>
+              </Link>
+            </>
+          ) : (
+            <form
+              action={async () => {
+                "use server";
+                await signIn("google");
+              }}
+            >
+              <button type="submit">Login</button>
+            </form>
+          )}
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
